1531db8cd595b834e386b3b57a446591ca614af1
wen <checkwen@126.com>
2025-09-06 22:36:37 +0800

    gubfxi

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,10 +1,14 @@
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
     tailwindcss: {
-      config: path.join(process.cwd(), 'tailwind.config.ts')
+      config: path.join(__dirname, 'tailwind.config.ts')
     },
     autoprefixer: {}, // Added autoprefixer
   },
